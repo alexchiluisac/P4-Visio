3727dc5dd4d572f740da9d87d24e830be38a69dd
alexchiluisac <ajchiluisa@wpi.edu>
2022-12-01 00:23:23 -0500

    Got stamped_traj_txt

---
+++ build/src/CMakeFiles/_pangolin.dir/DependInfo.cmake
@@ -0,0 +1,127 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/fonts.c" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/fonts.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "linux=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "src/include"
+  "/usr/include/eigen3"
+  "/usr/include/python3.8"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "/usr/include/OpenEXR"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/console/ConsoleView.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/console/ConsoleView.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/display/device/display_x11.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/display/device/display_x11.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/display/display.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/display/display.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/display/image_view.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/display/image_view.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/display/opengl_render_state.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/display/opengl_render_state.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/display/view.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/display/view.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/display/viewport.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/display/viewport.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/display/widgets/widgets.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/display/widgets/widgets.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/gl/glchar.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/gl/glchar.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/gl/gldraw.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/gl/gldraw.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/gl/glfont.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/gl/glfont.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/gl/glpangoglu.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/gl/glpangoglu.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/gl/gltext.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/gl/gltext.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/gl/gltexturecache.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/gl/gltexturecache.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/handler/handler.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/handler/handler.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/handler/handler_glbuffer.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/handler/handler_glbuffer.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/handler/handler_image.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/handler/handler_image.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io_exr.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io_exr.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io_jpg.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io_jpg.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io_pango.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io_pango.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io_png.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io_png.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io_ppm.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io_ppm.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io_raw.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io_raw.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io_tga.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io_tga.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/image_io_zstd.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/image_io_zstd.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/image/pixel_format.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/image/pixel_format.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/log/packet.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/log/packet.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/log/packetstream.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/log/packetstream.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/log/packetstream_reader.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/log/packetstream_reader.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/log/packetstream_writer.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/log/packetstream_writer.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/log/playback_session.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/log/playback_session.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/plot/datalog.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/plot/datalog.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/plot/plotter.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/plot/plotter.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/python/PyInterpreter.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/python/PyInterpreter.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/python/PyModulePangolin.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/python/PyModulePangolin.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/tools/video_viewer.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/tools/video_viewer.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/file_extension.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/file_extension.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/file_utils.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/file_utils.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/posix/condition_variable.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/posix/condition_variable.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/posix/semaphore.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/posix/semaphore.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/posix/shared_memory_buffer.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/posix/shared_memory_buffer.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/sigstate.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/sigstate.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/threadedfilebuf.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/threadedfilebuf.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/timer.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/timer.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/utils/uri.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/utils/uri.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/var/input_record_repeat.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/var/input_record_repeat.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/var/vars.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/var/vars.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/debayer.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/debayer.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/deinterlace.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/deinterlace.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/ffmpeg.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/ffmpeg.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/firewire.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/firewire.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/images.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/images.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/images_out.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/images_out.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/join.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/join.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/json.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/json.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/merge.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/merge.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/mirror.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/mirror.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/openni.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/openni.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/openni2.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/openni2.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/pango.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/pango.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/pango_video_output.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/pango_video_output.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/pvn.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/pvn.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/shift.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/shift.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/split.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/split.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/test.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/test.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/thread.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/thread.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/unpack.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/unpack.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/drivers/v4l.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/drivers/v4l.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/stream_encoder_factory.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/stream_encoder_factory.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/video.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/video.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/video_input.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/video_input.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/video_interface_factory.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/video_interface_factory.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/video_output.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/video_output.cpp.o"
+  "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/src/video/video_output_interface_factory.cpp" "/home/alexchiluisac/PycharmProjects/P4-Visio/pangolin/build/src/CMakeFiles/_pangolin.dir/video/video_output_interface_factory.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "linux=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "src/include"
+  "/usr/include/eigen3"
+  "/usr/include/python3.8"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "/usr/include/OpenEXR"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
